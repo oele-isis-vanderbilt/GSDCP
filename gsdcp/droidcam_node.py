148ea8899019bee9a2ce9f8074fde89636d2f5eb
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-09-28 19:57:01 -0500

    Force HD and scale

--- gsdcp/droidcam_node.py
+++ gsdcp/droidcam_node.py
@@ -1,6 +1,7 @@
-import time
+from typing import Optional
 
 import cv2
+import imutils
 from chimerapy.engine import DataChunk, Node
 from chimerapy.orchestrator import source_node
 
@@ -12,6 +13,7 @@ class DroidCam(Node):
         phone_ip: str,
         droidcam_port: int = 4747,
         frame_key: str = "frame",
+        force_suffix: Optional[str] = "/force/1920x1080",
         save_name: str = None,
         name: str = "DroidCam",
     ):
@@ -20,10 +22,14 @@ class DroidCam(Node):
         self.droidcam_port = droidcam_port
         self.frame_key = frame_key
         self.save_name = save_name
+        self.force_suffix = force_suffix
         self.cap = None
 
     def setup(self):
         addr = f"http://{self.phone_ip}:{self.droidcam_port}/video"
+        if self.force_suffix is not None:
+            addr += self.force_suffix
+
         self.logger.info(f"Connecting to DroidCam... at {addr}")
         self.cap = cv2.VideoCapture(addr)
 
@@ -36,20 +42,25 @@ class DroidCam(Node):
             ret, frame = self.cap.read()
 
         data_chunk = DataChunk()
-        data_chunk.add(self.frame_key, frame, "image")
 
         if self.save_name is not None:
             self.save_video(self.save_name, frame, 30)
-        time.sleep(1 / 35)
+
+        frame = imutils.resize(frame, width=640)
+        data_chunk.add(self.frame_key, frame, "image")
+
         return data_chunk
 
     def retry_capture(self):
         if self.cap is not None:
             self.cap.release()
         self.logger.info("Retrying to connect to DroidCam...")
-        self.cap = cv2.VideoCapture(
-            f"http://{self.phone_ip}:{self.droidcam_port}/video"
-        )
+        addr = f"http://{self.phone_ip}:{self.droidcam_port}/video"
+
+        if self.force_suffix is not None:
+            addr += self.force_suffix
+
+        self.cap = cv2.VideoCapture(addr)
 
     def teardown(self):
         self.cap.release()

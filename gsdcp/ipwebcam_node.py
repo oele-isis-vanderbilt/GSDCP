7795a36b1bc56268dbde8bc247bf9ab2537a9400
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-09-29 17:39:29 -0500

    Add video stream node

--- gsdcp/ipwebcam_node.py
+++ gsdcp/ipwebcam_node.py
@@ -5,6 +5,40 @@ import numpy as np
 
 from chimerapy.orchestrator import source_node
 
+import cv2
+import threading
+
+
+class VideoStream:
+    def __init__(self, url):
+        self.cap = cv2.VideoCapture(url)
+        self.ret = False
+        self.frame = None
+        self.lock = threading.Lock()
+        self.read_thread = threading.Thread(target=self.update)
+        self.read_thread.daemon = True
+        self.stop_event = threading.Event()
+        self.stop_event.clear()
+        self.read_thread.start()
+
+    def update(self):
+        while not self.stop_event.is_set():
+            ret, frame = self.cap.read()
+            with self.lock:
+                self.ret = ret
+                self.frame = frame
+
+    def read(self):
+        with self.lock:
+            ret = self.ret
+            frame = self.frame.copy() if self.frame is not None else None
+        return ret, frame
+
+    def teardown(self):
+        self.stop_event.set()
+        self.cap.release()
+
+
 @source_node(name="GSDCP_IPWebCam")
 class IPWebCam(Node):
     def __init__(self, rtsp_url: str, save_name: str = None, name: str = "IPWebCam"):
@@ -14,28 +48,20 @@ class IPWebCam(Node):
         self.save_name = save_name
 
     def setup(self):
-        self.cap = cv2.VideoCapture(self.rtsp_url)
+        self.cap = VideoStream(self.rtsp_url)
 
     def step(self) -> DataChunk:
         ret, frame = self.cap.read()
-        data_chunk = DataChunk()
-        if not ret:
-            self.logger.error("Failed to capture frame from IPWebCam.")
-            data_chunk.add("frame", np.random.randint(255, size=(640, 480, 3), dtype=np.uint8), "image")
-            return data_chunk
 
         if self.save_name is not None:
             self.save_video(self.save_name, frame, 30)
 
+        data_chunk = DataChunk()
         small_image = imutils.resize(frame, width=640)
         data_chunk.add("frame", small_image, "image")
+
         return data_chunk
 
-    def retry_capture(self):
-        if self.cap is not None:
-            self.cap.release()
-        self.logger.info("Retrying to connect to IPWebCam...")
-        self.cap = cv2.VideoCapture(self.rtsp_url)
-
     def teardown(self):
-        self.cap.release()
+        if self.cap is not None:
+            self.cap.teardown()

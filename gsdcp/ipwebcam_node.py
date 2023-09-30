772505d29fa8ade3fe8c1f93a7f25515a291831d
Umesh <umesh.timalsina@vanderbilt.edu>
2023-09-29 20:59:40 -0500

    Add requests based streaming node

--- gsdcp/ipwebcam_node.py
+++ gsdcp/ipwebcam_node.py
@@ -1,32 +1,62 @@
+import threading
+
 import cv2
-from chimerapy.engine import Node, DataChunk
 import imutils
 import numpy as np
-
+import requests
+from chimerapy.engine import DataChunk, Node
 from chimerapy.orchestrator import source_node
-
-import cv2
-import threading
+from requests.adapters import HTTPAdapter
 
 
 class VideoStream:
-    def __init__(self, url):
-        self.cap = cv2.VideoCapture(url)
+    def __init__(self, url, retries=5, timeout=5):
+        self.cap = None
+        self.url = url
+        self.retries = retries
+        self.timeout = timeout
         self.ret = False
-        self.frame = None
+        self.frame = np.random.randint(0, 255, (480, 640, 3), dtype=np.uint8)
         self.lock = threading.Lock()
         self.read_thread = threading.Thread(target=self.update)
         self.read_thread.daemon = True
         self.stop_event = threading.Event()
         self.stop_event.clear()
+        self.session = None
+
+    def setup(self):
+        self.session = requests.Session()
+        # Setup Retry Strategy
+        retry = HTTPAdapter(max_retries=self.retries)
+        self.session.mount(self.url, retry)
+        self.cap = self.session.get(self.url, stream=True, timeout=self.timeout)
+        assert (
+            self.cap.status_code == 200
+        ), f"Received unexpected status code {self.cap.status_code}"
         self.read_thread.start()
 
     def update(self):
         while not self.stop_event.is_set():
-            ret, frame = self.cap.read()
-            with self.lock:
-                self.ret = ret
-                self.frame = frame
+            byte_stream = bytes()
+            for chunk in self.cap.iter_content(chunk_size=1024):
+                byte_stream += chunk
+                a = byte_stream.find(b"\xff\xd8")
+                b = byte_stream.find(b"\xff\xd9")
+
+                if a != -1 and b != -1:
+                    jpg = byte_stream[a : b + 2]
+                    byte_stream = byte_stream[b + 2 :]
+
+                    try:
+                        i = cv2.imdecode(
+                            np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR
+                        )
+                        with self.lock:
+                            self.ret = i is not None
+                            self.frame = i
+                    except Exception as e:
+                        print(f"Error encountered: {e}")
+                        i = None
 
     def read(self):
         with self.lock:
@@ -36,12 +66,14 @@ class VideoStream:
 
     def teardown(self):
         self.stop_event.set()
-        self.cap.release()
+        self.session.close()
 
 
 @source_node(name="GSDCP_IPWebCam")
 class IPWebCam(Node):
-    def __init__(self, rtsp_url: str, save_name: str = None, name: str = "IPWebCam"):
+    def __init__(
+        self, rtsp_url: str, save_name: str = None, name: str = "IPWebCam"
+    ):
         super().__init__(name=name)
         self.rtsp_url = rtsp_url
         self.cap = None
@@ -49,6 +81,7 @@ class IPWebCam(Node):
 
     def setup(self):
         self.cap = VideoStream(self.rtsp_url)
+        self.cap.setup()
 
     def step(self) -> DataChunk:
         ret, frame = self.cap.read()

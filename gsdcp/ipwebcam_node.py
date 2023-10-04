ab53f299ddac20d1a79ec5874216d9dacacd45eb
Umesh <umesh.timalsina@vanderbilt.edu>
2023-10-03 20:11:39 -0500

    OpenCV at last

--- gsdcp/ipwebcam_node.py
+++ gsdcp/ipwebcam_node.py
@@ -1,38 +1,74 @@
+import cv2
 import imutils
+import requests
 from chimerapy.engine import DataChunk, Node
 from chimerapy.orchestrator import source_node
-from vidgear.gears import CamGear
 
 
 @source_node(name="GSDCP_IPWebCam")
 class IPWebCam(Node):
     def __init__(
-        self, rtsp_url: str, save_name: str = None, name: str = "IPWebCam"
+        self,
+        url: str,
+        tag: str = "",
+        save_name: str = None,
+        name: str = "IPWebCamRecorder",
     ):
         super().__init__(name=name)
-        self.rtsp_url = rtsp_url
+        self.url: str = url
         self.cap = None
+        self.tag = tag
         self.save_name = save_name
         self.started = False
 
     def setup(self):
-        self.cap = CamGear(self.rtsp_url)
+        self.cap = cv2.VideoCapture(f"{self.url}/video")
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
     def step(self) -> DataChunk:
         if not self.started:
-            print("starting")
-            self.cap.start()
-            self.started = True
+            self._send_start_recording_request()
         data_chunk = DataChunk()
-        frame = self.cap.read()
+        ret, frame = self.cap.read()
+
+        if not ret:
+            return None
 
         if self.save_name is not None:
-            self.save_video(self.save_name, frame, self.cap.framerate)
+            self.save_video(
+                self.save_name, frame, self.cap.get(cv2.CAP_PROP_FPS)
+            )
 
         small_frame = imutils.resize(frame, width=640)
         data_chunk.add("frame", small_frame, "image")
+
+        if self.state.fsm == "STOPPED":
+            self._send_stop_recording_request()
+
         return data_chunk
 
+    def _send_start_recording_request(self):
+        path = (
+            "/startvideo?force=1"
+            if self.tag == ""
+            else f"/startvideo?tag={self.tag}&force=1"
+        )
+        response = requests.get(f"{self.url}{path}")
+        assert (
+            response.status_code == 200
+        ), f"Failed to start recording: {response.text}"
+        self.started = True
+        self.logger.info("Started recording")
+
+    def _send_stop_recording_request(self):
+        path = "/stopvideo?force=1"
+        response = requests.get(f"{self.url}{path}")
+        assert (
+            response.status_code == 200
+        ), f"Failed to stop recording: {response.text}"
+        self.started = False
+        self.logger.info("Stopped recording")
+
     def teardown(self):
-        if self.started:
-            self.cap.stop()
+        if self.cap is not None:
+            self.cap.release()

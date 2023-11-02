8eaea39320742eb06adceaf25443681107b05d66
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-11-02 14:23:19 -0500

    Fix time alignment issue in audio (#6)

--- gsdcp/umc1820_node.py
+++ gsdcp/umc1820_node.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from multiprocessing import Queue
-from typing import Optional
+from queue import Empty
+from typing import Dict, Optional
 
 import numpy as np
 import pyaudio
 from chimerapy.engine import DataChunk, Node
+from chimerapy.engine.eventbus import TypedObserver
 from chimerapy.orchestrator import source_node
 
 
@@ -21,7 +24,7 @@ class UMC1820(Node):
     """
 
     SAMPLE_RATE = 44100
-    CHUNK_SIZE = 512
+    CHUNK_SIZE = 4096
     CHANNELS = 10
 
     def __init__(self, name="GSDCP_UMC1820", save_name=None, chunk_key="audio"):
@@ -32,8 +35,9 @@ class UMC1820(Node):
         self.started = False
         self.audio_queue: Optional[Queue] = None
         self.chunk_key = chunk_key
+        self.observers: Dict[str, TypedObserver] = {}
 
-    def setup(self) -> None:
+    async def setup(self) -> None:
         """Setup the UMC1820 for recording.
 
         This method will setup the UMC1820 for recording. It will also
@@ -58,20 +62,52 @@ class UMC1820(Node):
         self.audio_queue = Queue()
         self.logger.info("UMC1820 setup complete.")
 
+        self.observers = {
+            "NodeState.changed": TypedObserver(
+                "NodeState.changed",
+                on_asend=self.a_start_stop_stream,
+                handle_event="drop",
+            )
+        }
+
+        for ob in self.observers.values():
+            await self.eventbus.asubscribe(ob)
+
+    async def a_start_stop_stream(self):
+        self.logger.info(
+            f"{self.__class__.__name__} state changed to {self.state.fsm} at: "
+            f"{datetime.now().isoformat()}."
+        )
+        if self.state.fsm == "PREVIEWING":
+            self.stream.start_stream()
+            self.started = True
+            self.logger.info(
+                f"UMC1820 streaming started. State is {self.state.fsm}"
+            )
+
+        elif self.state.fsm == "STOPPED":
+            self.stream.stop_stream()
+            self.started = False
+            self.logger.info(
+                f"UMC1820 streaming stopped. State is {self.state.fsm}"
+            )
+
     def step(self) -> DataChunk:
         """Step the UMC1820.
 
         This method will step the UMC1820 and return the recorded data.
         """
-        if not self.started:
-            self.stream.start_stream()
-            self.started = True
-
-        audio_data = self.audio_queue.get()
-        ret_chunk = DataChunk()
-        ret_chunk.add(self.chunk_key, audio_data)
-
-        return ret_chunk
+        try:
+            audio_data = self.audio_queue.get(timeout=1)
+            ret_chunk = DataChunk()
+            ret_chunk.add(self.chunk_key, audio_data)
+            return ret_chunk
+        except Empty:
+            self.logger.debug("UMC1820 queue empty.")
+            return None
+        except Exception as e:
+            self.logger.error(f"{e.__class__.__name__} in UMC1820 step: {e}")
+            return None
 
     def _save_multi_channel(self, in_data, frame_count, time_info, status):
         """Save the multi-channel audio data.
@@ -80,37 +116,36 @@ class UMC1820(Node):
         """
         numpy_data = np.frombuffer(in_data, dtype=np.int16)
         channels_dict = {}
-
         for j in range(self.CHANNELS):
-            channel_data = numpy_data[j :: self.CHANNELS].tobytes()
+            channel_data = numpy_data[j :: self.CHANNELS]
             channels_dict[j] = channel_data
             if self.save_name is not None:
                 self.save_audio_v2(
                     name=f"{self.save_name}-input-{j+1}",
-                    data=channel_data,
+                    data=channel_data.tobytes(),
                     channels=1,
                     sampwidth=2,
                     framerate=self.SAMPLE_RATE,
                     nframes=self.CHUNK_SIZE,
                 )
 
-        self.save_audio_v2(
-            name=f"{self.save_name}-combined",
-            data=in_data,
-            channels=self.CHANNELS,
-            sampwidth=2,
-            framerate=self.SAMPLE_RATE,
-            nframes=self.CHUNK_SIZE,
-        )
+        if self.save_name is not None:
+            self.save_audio_v2(
+                name=f"{self.save_name}-combined",
+                data=in_data,
+                channels=self.CHANNELS,
+                sampwidth=2,
+                framerate=self.SAMPLE_RATE,
+                nframes=self.CHUNK_SIZE,
+            )
 
         self.audio_queue.put_nowait(channels_dict)
 
         return None, pyaudio.paContinue
 
     def teardown(self) -> None:
-        self.stream.stop_stream()
-        self.stream.close()
-        self.started = False
+        self.audio.terminate()
+        self.logger.info("UMC1820 teardown complete.")
 
     @staticmethod
     def _locate_device() -> int:
